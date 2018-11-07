012e5f11e44d4bbcc7e11af52021c4f889906231
Tristan Burgess <tburgessdev@gmail.com>
2018-11-07 17:58:26 -0500

    :sparkles: refactor: wrangle unified controller code into GameInput module

--- src/scenes/MovementTest.ts
+++ src/scenes/MovementTest.ts
@@ -1,141 +1,15 @@
-import * as debug from "debug"
+import * as Debug from "debug"
 import * as Phaser from "phaser"
 
+import UnifiedController from "../GameInput"
 import { Assets } from "../assets"
 
 import { Scenes } from "./"
 
-const log = debug(`game:scenes:${Scenes.MovementTest}`)
-
-class GamepadAxisRange {
-    private axis: Phaser.Input.Gamepad.Axis
-    private comparator: string
-    private threshold: number
-    private value: number
-
-    constructor(axis: Phaser.Input.Gamepad.Axis, comparator: string, threshold: number) {
-        this.axis = axis
-        this.comparator = comparator
-        this.threshold = threshold
-        this.value = 0.0
-    }
-
-    public getMagnitude() {
-        this.poll()
-        return Math.abs(this.value)
-    }
-
-    public valueInRange() {
-        this.poll()
-
-        let inRange: boolean = false
-
-        switch (this.comparator) {
-            case "lt": {
-                inRange = this.value < this.threshold
-                break
-            }
-            case "gt": {
-                inRange = this.value > this.threshold
-                break
-            }
-            case "le": {
-                inRange = this.value <= this.threshold
-                break
-            }
-            case "ge": {
-                inRange = this.value >= this.threshold
-                break
-            }
-            case "eq": {
-                inRange = this.value === this.threshold
-                break
-            }
-            case "ne": {
-                inRange = this.value !== this.threshold
-                break
-            }
-        }
-
-        return inRange
-    }
-
-    private poll() {
-        this.value = this.axis.getValue()
-    }
-}
-
-class VirtualKey {
-    private curDownState: boolean
-    private gamepadButton: Phaser.Input.Gamepad.Button | undefined
-    private gamepadAxisRange: GamepadAxisRange | undefined
-    private keyboardButton: Phaser.Input.Keyboard.Key | undefined
-    private magnitude: number
-    private prevDownState: boolean
-
-    constructor(keyboardButton: Phaser.Input.Keyboard.Key) {
-        this.keyboardButton = keyboardButton
-        this.curDownState = false
-        this.prevDownState = false
-        this.magnitude = 0.0
-    }
-
-    public getMagnitude() {
-        this.poll()
-        return this.magnitude
-    }
-
-    public isDown() {
-        this.poll()
-        return this.curDownState
-    }
-
-    public isUniquelyDown() {
-        this.poll()
-        return this.curDownState && !this.prevDownState
-    }
-
-    public setGamepadButton(button: Phaser.Input.Gamepad.Button) {
-        this.gamepadButton = button
-    }
-
-    public setGamepadAxisRange(axis: Phaser.Input.Gamepad.Axis, comparator: string, threshold: number) {
-        this.gamepadAxisRange = new GamepadAxisRange(axis, comparator, threshold)
-    }
-
-    private poll() {
-        const kbdButtonIsDown: boolean = this.keyboardButton === undefined ? false : this.keyboardButton.isDown
-        const gpadButtonIsDown: boolean = this.gamepadButton === undefined ? false : this.gamepadButton.pressed
-        const gpadStickIsDown: boolean =
-            this.gamepadAxisRange === undefined ? false : this.gamepadAxisRange.valueInRange()
-
-        const prev: boolean = this.curDownState
-        const cur: boolean = kbdButtonIsDown || gpadButtonIsDown || gpadStickIsDown
-
-        this.prevDownState = prev
-        this.curDownState = cur
-
-        if (gpadStickIsDown) {
-            this.magnitude = this.gamepadAxisRange!.getMagnitude()
-        } else if (this.isDown) {
-            this.magnitude = 1.0
-        } else {
-            this.magnitude = 0.0
-        }
-    }
-}
-
-interface IGameController {
-    up?: VirtualKey
-    down?: VirtualKey
-    left?: VirtualKey
-    right?: VirtualKey
-    actionLB?: VirtualKey
-    actionRB?: VirtualKey
-}
+const log = Debug(`game:scenes:${Scenes.MovementTest}`)
 
 class MovementTest extends Phaser.Scene {
-    private controller: IGameController
+    private controller: UnifiedController
     private player: Phaser.Physics.Arcade.Sprite
     private playerProjectiles: Phaser.Physics.Arcade.Group
     private worldTop: Phaser.Physics.Arcade.Sprite
@@ -162,32 +36,7 @@ class MovementTest extends Phaser.Scene {
         const midX = this.cameras.main.width / 2
         const midY = this.cameras.main.height / 2
 
-        this.controller = {
-            actionLB: new VirtualKey(this.input.keyboard.addKey("LEFT")),
-            actionRB: new VirtualKey(this.input.keyboard.addKey("RIGHT")),
-            down: new VirtualKey(this.input.keyboard.addKey("S")),
-            left: new VirtualKey(this.input.keyboard.addKey("A")),
-            right: new VirtualKey(this.input.keyboard.addKey("D")),
-            up: new VirtualKey(this.input.keyboard.addKey("W"))
-        }
-
-        this.input.gamepad.once(
-            "down",
-            (pad: Phaser.Input.Gamepad.Gamepad, button: Phaser.Input.Gamepad.Button, index: number) => {
-                // TODO(tristan): some better way to do this other than guesstimating button indicies?
-                this.controller.actionLB!.setGamepadButton(pad.buttons[4])
-                this.controller.actionRB!.setGamepadButton(pad.buttons[5])
-                this.controller.down!.setGamepadButton(pad.buttons[13])
-                this.controller.down!.setGamepadAxisRange(pad.axes[1], "gt", 0)
-                this.controller.left!.setGamepadButton(pad.buttons[14])
-                this.controller.left!.setGamepadAxisRange(pad.axes[0], "lt", 0)
-                this.controller.right!.setGamepadButton(pad.buttons[15])
-                this.controller.right!.setGamepadAxisRange(pad.axes[0], "gt", 0)
-                this.controller.up!.setGamepadButton(pad.buttons[12])
-                this.controller.up!.setGamepadAxisRange(pad.axes[1], "lt", 0)
-            },
-            this
-        )
+        this.controller = new UnifiedController(this.input)
 
         this.add.image(midX, midY, Assets.Background)
 
